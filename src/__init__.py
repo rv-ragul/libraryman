64b2dbce1a7b5ae5fb769856d692a8d83cebd844
Ragul R <ragul542rv@gmail.com>
2023-10-08 18:48:00 +0530

    index file, favicon

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,32 @@
-from flask import Flask, g, redirect, render_template, session, url_for
+import os
 
-from . import database
-from .views import auth
+from flask import (
+    Flask,
+    render_template,
+    send_from_directory,
+)
+
+from src import database
+from src.views import auth
+from src.views import books
 
 
 app = Flask(__name__)
 
 app.config.from_pyfile("config.py", silent=True)
-
-app.register_blueprint(auth.bp)
 database.init_app(app)
 
 
-    return app
+@app.get("/")
+@auth.login_required
+def index():
+    return render_template("index.html")
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico")
+
+
+app.register_blueprint(auth.bp)
+app.register_blueprint(books.bp)
