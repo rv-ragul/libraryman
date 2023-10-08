21a2a20b67230253a7035865a964a6216c2a4019
Ragul R <ragul542rv@gmail.com>
2023-10-08 19:29:18 +0530

    view books at /books

--- src/views/books.py
+++ src/views/books.py
@@ -1,15 +1,24 @@
 import json.tool
 from typing import Dict
 
-from flask import Blueprint, flash, json, render_template, request
+from flask import Blueprint, flash, json, render_template, request, url_for
 import httpx
 from sqlalchemy.exc import IntegrityError
 
 from src.database import get_db
 from src.models import Book, Issued
-from src.views.auth import login_required
+from auth import login_required
 
 bp = Blueprint("books", __name__, url_prefix="/books")
+
+
+@bp.get("/")
+@login_required
+def view_books():
+    """View books in the Library and search them by author and book name"""
+
+    books = Book.query.all()
+    return render_template("books/view.html", books=books)
 
 
 @bp.route("/issue", methods=["GET", "POST"])
@@ -33,7 +42,7 @@ def issue():
             flash("Books database updated successfully")
         else:
             flash(error)
-    return render_template("issue.html")
+    return render_template("books/issue.html")
 
 
 @bp.route("/return")
@@ -41,16 +50,7 @@ def issue():
 def return_book():
     if request.method == "POST":
         pass
-    return render_template("return.html")
-
-
-@bp.get("/view")
-@login_required
-def view_books():
-    """View books in the Library and search them by author and book name"""
-    books = Book.query.all()
-
-    return render_template("books/view.html", books=books)
+    return render_template("books/return.html")
 
 
 @bp.route("/import", methods=["GET", "POST"])
@@ -73,4 +73,4 @@ def import_books():
             db.add_all([Book(book) for book in books])
             db.commit()
 
-    return render_template("books/import.html")
+    return render_template(url_for("books.view_books"))
